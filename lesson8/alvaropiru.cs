ef759ef2a2a688cedfb1b6f30cc716f9f5a14470
masterjohn12 <lopogax@gmail.com>
2017-05-30 22:59:35 -0300

    gabriel

---
+++ lesson8/alvaropiru.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static double calcularINSS(double sal)
+        {
+            double salario = sal;
+            if (salario <= 1659.38f)
+            {
+                salario = sal + (sal * 0.08);
+            }else if(salario >= 1659.39f && salario <= 2765.66f)
+            {
+                salario = sal + (sal * 0.09);
+            }
+            else if(salario >= 2765.67f && salario <= 5531.31f)
+            {
+                salario = sal + (sal * 0.11);
+            }
+            else
+            {
+                salario = sal - 608.44f;
+            }
+            return salario;
+        }
+
+        static double calcularHorasExtras(double sal, int horas)
+        {
+
+            return (((sal / 220) * 0.5) + (sal / 220))*horas;
+        }
+
+        static double calcularIRPF(double sal)
+        {
+            double salario = sal;
+            if(sal >= 1903.99f && sal <= 2826.65)
+            {
+                salario = (sal + (sal * 0.075))-142.80f;
+            }
+            else
+            if (sal >= 2826.66 && sal <= 3751.05f)
+            {
+                salario = (sal + (sal * 0.15))-354.80f;
+            }
+            else
+            if (sal >= 3751.06f && sal <= 4664.68f)
+            {
+                salario = (sal + (sal * 0.225))-636.13f;
+            }
+            else
+            {
+                salario = (sal + (sal * 0.275))-869.36f;
+            }
+
+            return salario;
+        }
+
+        static void Main(string[] args)
+        {
+
+            double salariob;
+            double horasExtras;
+            double gratificacao;
+            double insalu;
+            string nome;
+            string matricula;
+            string cargo;
+            string setor;
+
+            bool test;
+
+            Console.WriteLine("Nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Matricula: ");
+            matricula = Console.ReadLine();
+            Console.WriteLine("Cargo: ");
+            cargo = Console.ReadLine();
+            Console.WriteLine("Setor: ");
+            setor = Console.ReadLine();
+
+
+            do
+            {
+                Console.WriteLine("Digite o salario base");
+                test = double.TryParse(Console.ReadLine(), out salariob);
+
+            } while (test == false);
+
+            do
+            {
+                Console.WriteLine("Horas extras realizadas no mes");
+                test = double.TryParse(Console.ReadLine(), out horasExtras);
+
+            } while (test == false);
+
+            do
+            {
+                Console.WriteLine("Gratificação de função");
+                test = double.TryParse(Console.ReadLine(), out gratificacao);
+
+            } while (test == false);
+
+            do
+            {
+                Console.WriteLine("Adicional de insalubridade");
+                test = double.TryParse(Console.ReadLine(), out insalu);
+
+            } while (test == false);
+
+            do
+            {
+                Console.WriteLine("Adicional de periculosidade");
+                test = double.TryParse(Console.ReadLine(), out insalu);
+
+            } while (test == false);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
